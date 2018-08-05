ae0226edca5d03eadbe980b5d1f584aff6f780c2
junyeongbluefin <junyeong@bluefinenterprises.com>
2018-08-04 21:50:00 -0400

    add result of tpot

--- src/estimator-tpot-automl-model-adopt.py
+++ src/estimator-tpot-automl-model-adopt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# 0. Import  
+# 0. Import
 import numpy as np
 import pandas as pd
 import sklearn.preprocessing as pp
@@ -11,26 +11,12 @@ import sklearn.metrics as metrics
 # Load Data Sets
 df = pd.read_csv('../input/train.csv')
 df = df.set_index('Id')
-
-#sdf = pd.read_csv('../input/test.csv')
-#sdf = sdf.set_index('Id')
-#print(df.head())
-
-#print(df.Street)
-#print (df.columns.values)
-
 y = df.SalePrice
-print("Average sale price: " + "${:,.0f}".format(y.mean()))
 
-# Combine test and train for preprocessing
-#df = df.drop('SalePrice', axis=1)
-#all_df = df.append(sdf)
 all_df = df.drop('SalePrice', axis=1)
+
 df = all_df.iloc[:1000, :]
 sdf = all_df.iloc[1000:, :]
-print(all_df.shape)
-print(df.shape)
-print(sdf.shape)
 
 
 # Create lists of categorical vs numeric features
@@ -38,30 +24,13 @@ all_features = list(df.columns.values)
 numeric_features = ['LotFrontage', 'LotArea', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF','LowQualFinSF','GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr','KitchenAbvGr','TotRmsAbvGrd','TotalBsmtSF','Fireplaces', 'GarageCars', 'GarageArea','WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal']
 #numeric_features = list(df.select_dtypes(include=[np.number]).columns.values)
 categorical_features = [f for f in all_features if not (f in numeric_features)]
-print(len(all_features), len(categorical_features), len(numeric_features))
-
-# Preprocess numerical columns
 numeric_df = all_df[numeric_features]
-print(numeric_df.shape)
 
 # Impute
 X = numeric_df.values
-imp = pp.Imputer(missing_values='NaN', strategy='median', axis=0) # probably, median will be better
+imp = pp.Imputer(missing_values='NaN', strategy='most_frequent', axis=0) # probably, median will be better
 imp = imp.fit(X)
 X = imp.transform(X)
-print(X.shape)
-
-# Scale
-#print (X[0, :])
-scaler = pp.StandardScaler() # StandardScaler, MinMaxScaler
-scaler = scaler.fit(X)
-X = scaler.transform(X)
-#print (X[0, :])
-
-# PCA
-from sklearn.decomposition import PCA
-pca = PCA()
-X = pca.fit_transform(X)
 
 # Expand categorical into columns
 def process_categorical(ndf, df, categorical_features):
@@ -70,12 +39,19 @@ def process_categorical(ndf, df, categorical_features):
         new_cols.index = df.index
         ndf = pd.merge(ndf, new_cols, how = 'inner', left_index=True, right_index=True)
     return ndf
+
 numeric_df = pd.DataFrame(X)
 numeric_df.index = all_df.index
 combined_df = process_categorical(numeric_df, all_df, categorical_features)
+
 X = combined_df.values
 
 # Split Data Sets
+#x_train = df.values
+#y_train = y[:1000]
+#x_test = sdf.values
+#y_test = y[1000:]
+
 test_n = df.shape[0]
 x_train = X[:test_n,:]
 y_train = y[:test_n]
@@ -83,37 +59,35 @@ x_test = X[test_n:,:]
 y_test = y[test_n:]
 
 
-### Start Keras
-import keras
-from keras import metrics
-from keras import regularizers
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Activation
-from keras.layers import Conv2D, MaxPooling2D
-from keras.optimizers import Adam, RMSprop
-from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
-from keras.utils import plot_model
-from keras.models import load_model
+#import numpy as np
+#import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import RidgeCV
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
+
+# NOTE: Make sure that the class is labeled 'target' in the data file
+#tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+#features = tpot_data.drop('target', axis=1).values
+training_features, testing_features, training_target, testing_target = x_train, x_test, y_train, y_test #\
+#            train_test_split(features, tpot_data['target'].values, random_state=42)
+
+# Score on the training set was:-782999502.5452403
+model = make_pipeline(
+    StackingEstimator(estimator=RidgeCV()),
+    RandomForestRegressor(bootstrap=False, max_features=0.4, min_samples_leaf=5, min_samples_split=12, n_estimators=100)
+)
+
+model.fit(training_features, training_target)
+results = model.predict(testing_features)
+#print(results)
 
 
-kc_y_train = y_train
-kc_x_train = x_train
-kc_y_valid = y_test
-kc_x_valid = x_test
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+y_pred = model.predict(x_test[:10,])
+print (y_pred)
+print (y_test[:10])
+print('MLPRegressor Regression score is %f (traning)' % model.score(x_train, y_train))
+print('MLPRegressor Regression score is %f (test)' % model.score(x_test, y_test)) # 84%
 
 
--- src/estimator-tpot-automl-model.py
+++ src/estimator-tpot-automl-model.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.pipeline import make_pipeline, make_union
 from tpot.builtins import StackingEstimator
 
@@ -12,10 +12,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-790087137.7221434
+# Score on the training set was:-782999502.5452403
 exported_pipeline = make_pipeline(
     StackingEstimator(estimator=RidgeCV()),
-    KNeighborsRegressor(n_neighbors=88, p=1, weights="distance")
+    RandomForestRegressor(bootstrap=False, max_features=0.4, min_samples_leaf=5, min_samples_split=12, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
