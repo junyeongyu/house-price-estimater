c84b8752b2001de387ae4b0e8cd6d6fd2c830617
junyeongbluefin <junyeong@bluefinenterprises.com>
2018-08-04 23:57:37 -0400

    add keras

--- src/estimator-keras.py
+++ src/estimator-keras.py
@@ -49,7 +49,7 @@ X = numeric_df.values
 imp = pp.Imputer(missing_values='NaN', strategy='median', axis=0) # probably, median will be better
 imp = imp.fit(X)
 X = imp.transform(X)
-print(X.shape)
+#print(X.shape)
 
 # Scale
 #print (X[0, :])
@@ -96,24 +96,135 @@ from keras.utils import plot_model
 from keras.models import load_model
 
 
-kc_y_train = y_train
-kc_x_train = x_train
-kc_y_valid = y_test
-kc_x_valid = x_test
+arr_x_train = x_train
+arr_y_train = y_train
+#kc_y_valid = y_test
+#kc_x_valid = x_test
+
+#y_train = np.array(y_train)
+
+#print('Size of training set: ', len(kc_x_train))
+#print('Size of validation set: ', len(kc_x_valid))
+#print('Size of test set: ', len(kc_test), '(not converted)')
+
+def norm_stats(df1, df2):
+    dfs = df1.append(df2)
+    minimum = np.min(dfs)
+    maximum = np.max(dfs)
+    mu = np.mean(dfs)
+    sigma = np.std(dfs)
+    return (minimum, maximum, mu, sigma)
+
+def z_score(col, stats):
+    m, M, mu, s = stats
+    df = pd.DataFrame()
+    for c in col.columns:
+        df[c] = (col[c]-mu[c])/s[c]
+    return df
 
 
+def basic_model_x(x_size, y_size):
+    t_model = Sequential()
+    t_model.add(Dense(100, activation="tanh", input_shape=(x_size,)))
+    t_model.add(Dense(50, activation="relu"))
+    t_model.add(Dense(y_size))
+    t_model.compile(loss='mean_squared_error',
+        optimizer=Adam(),
+        metrics=[metrics.mae])
+    return(t_model)
+
+def basic_model_1(x_size, y_size):
+    t_model = Sequential()
+    t_model.add(Dense(100, activation="relu", input_dim=x_size))
+    t_model.add(Dense(50, activation="relu"))
+    t_model.add(Dense(y_size))
+    t_model.compile(loss='mean_squared_error',
+        optimizer=Adam(),
+        metrics=[metrics.mae])
+    return(t_model)
+
+def basic_model_2(x_size, y_size):
+    t_model = Sequential()
+    t_model.add(Dense(100, activation="relu", input_shape=(x_size,)))
+    t_model.add(Dropout(0.1))
+    t_model.add(Dense(50, activation="relu"))
+    t_model.add(Dense(20, activation="relu"))
+    t_model.add(Dense(y_size))
+    print(t_model.summary())
+    t_model.compile(loss='mean_squared_error',
+        optimizer=Adam(),
+        metrics=[metrics.mae])
+    return(t_model)
+
+def basic_model_3(x_size, y_size):
+    t_model = Sequential()
+    t_model.add(Dense(80, activation="relu", kernel_initializer='normal', input_shape=(x_size,)))
+    t_model.add(Dropout(0.3))
+    t_model.add(Dense(120, activation="relu", kernel_initializer='normal', kernel_regularizer=regularizers.l1(0.01), bias_regularizer=regularizers.l1(0.01)))
+    t_model.add(Dropout(0.3))
+    t_model.add(Dense(20, activation="relu", kernel_initializer='normal', kernel_regularizer=regularizers.l1_l2(0.01), bias_regularizer=regularizers.l1_l2(0.01)))
+    t_model.add(Dropout(0.3))
+    t_model.add(Dense(10, activation="relu", kernel_initializer='normal'))
+    t_model.add(Dropout(0.2))
+    t_model.add(Dense(y_size))
+    t_model.compile(
+        loss='mean_squared_error',
+        optimizer='nadam',
+        metrics=[metrics.mae])
+    return(t_model)
+
+print(x_train.shape[1])
+print(y_train.shape)
+
+# Make mode
+#model = basic_model_2(x_train.shape[1], 1)
+#model.summary()
+
+def make_model():
+    return basic_model_3(x_train.shape[1], 1)
 
 
+epochs = 1000 #500
+batch_size = 1000
+print('Epochs: ', epochs)
+print('Batch size: ', batch_size)
 
+keras_callbacks = [
+    # ModelCheckpoint('/tmp/keras_checkpoints/model.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss', save_best_only=True, verbose=2)
+    # ModelCheckpoint('/tmp/keras_checkpoints/model.{epoch:02d}.hdf5', monitor='val_loss', save_best_only=True, verbose=0)
+    # TensorBoard(log_dir='/tmp/keras_logs/model_3', histogram_freq=0, write_graph=True, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None),
+    EarlyStopping(monitor='val_mean_absolute_error', patience=80, verbose=0) # 20
+]
+print(x_train.shape)
 
+#keras.wrappers.scikit_learn.KerasRegressor
 
+from keras.wrappers.scikit_learn import KerasRegressor
+model = KerasRegressor(build_fn=make_model, epochs=epochs, batch_size=batch_size, verbose=True, callbacks=keras_callbacks)
+model.fit(x_train, y_train)
 
+'''
+history = model.fit(x_train, y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    shuffle=True,
+    verbose=2,#0, # Change it to 2, if wished to observe execution
+    #validation_data=(arr_x_valid, arr_y_valid),
+    callbacks=keras_callbacks)
+'''
+y_pred = model.predict(x_test[:20,])
 
+print (y_pred)
+print (y_test[:20])
+print('KERAS score is %f (traning)' % model.score(x_train, y_train))
+print('KERAS score is %f (test)' % model.score(x_test, y_test)) # ??%
 
-
-
-
-
-
-
+'''
+from sklearn.metrics import mean_squared_error
+score = mean_squared_error(y_test, model.predict(x_test))
+print(score)
+from sklearn.metrics import mean_absolute_error
+score = mean_absolute_error(y_test, model.predict(x_test))
+print(score)
+'''
 
